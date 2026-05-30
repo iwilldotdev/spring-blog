393409ab3b11c65e0cf15e58ff4020f7847cdfac
William Gonçalves <euwilliamgoncalves@gmail.com>
2026-05-30 13:15:36 -0300

    chore: add controllers and services package scaffolding

---
+++ src/main/java/com/descomplica/spring_blog/controllers/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Controllers REST da API do mini blog.
+ */
+package com.descomplica.spring_blog.controllers;
---
+++ src/main/java/com/descomplica/spring_blog/services/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Serviços de regra de negócio do mini blog.
+ */
+package com.descomplica.spring_blog.services;
